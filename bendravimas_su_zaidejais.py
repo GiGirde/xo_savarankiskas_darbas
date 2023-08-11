90f1da6b2f4edc808b02fe2c101438c6541f4a47
Giedre.girdauskaite <giedre.girdauskaite@gmail.com>
2023-08-11 11:08:44 +0300

    + sukurtas bendravimas su žaidėju

---
+++ bendravimas_su_zaidejais.py
@@ -0,0 +1,21 @@
+# čia yra bendravimas su žaidėju
+
+
+print("Sveiki! Čia yra žaidimas XO!")
+print("--------------------------------")
+
+x_žaidėjas = (input("Žaidėjas X - Prisistatykite: "))
+o_žaidėjas = (input("Žaidėjas O - Prisistatykite: "))
+print("--------------------------------")
+print("Malonu, " + x_žaidėjas, "ir " + o_žaidėjas, "susipažinti")
+print("--------------------------------")
+sutikimas= input("Ar norite pradėti žaidimą? pasirinkite 'taip' arba 'ne'")
+print("************************************")
+
+if sutikimas.lower() == "taip":
+    print("ŽAIDIMASD PRASIDEDA")
+    print("************************************")
+
+    # lenta()
+else:
+    print("labai gaila")
